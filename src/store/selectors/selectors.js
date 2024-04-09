e7c0c61b7ceb6c46c3222317f5964ab5a19df39e
Julia Simakina <simakina.js@yandex.ru>
2024-04-10 00:20:25 +0300

    refactor: For loop in selectors

--- src/store/selectors/selectors.js
+++ src/store/selectors/selectors.js
@@ -1,24 +1,41 @@
-import { createSelector } from "reselect";
-import { SHOW_ALL, SHOW_ACTIVE } from "../actions/actionNames";
-export const getTodos = (state) => state.todos.todoList;
-export const getVisibilityFilter = (state) => state.todos.currentFilter;
+import { createSelector } from 'reselect';
+import { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } from '../actions/actionNames';
+export const getTodos = state => state.todos.todoList;
+export const getVisibilityFilter = state => state.todos.currentFilter;
 
 export const filteredTodosSelector = createSelector(
   [getTodos, getVisibilityFilter],
   (todos, visibilityFilter) => {
-    const filters = {
-      SHOW_COMPLETED: (todo) => todo.completed,
-      SHOW_ACTIVE: (todo) => !todo.completed,
-      SHOW_ALL: () => true,
-    };
-    let filteredTodos = todos.filter(
-      filters[visibilityFilter] || filters[SHOW_ALL]
-    );
-    let activeTodosCount = todos.filter(filters[SHOW_ACTIVE]).length;
+    let filteredTodos = [];
+    let activeTodosCount = 0;
+
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos[i];
+
+      switch (visibilityFilter) {
+        case SHOW_COMPLETED:
+          if (todo.completed) {
+            filteredTodos.push(todo);
+          }
+          break;
+        case SHOW_ACTIVE:
+          if (!todo.completed) {
+            activeTodosCount++;
+            filteredTodos.push(todo);
+          }
+          break;
+        case SHOW_ALL:
+        default:
+          filteredTodos.push(todo);
+          if (!todo.completed) {
+            activeTodosCount++;
+          }
+      }
+    }
 
     return {
       filteredTodos: filteredTodos,
-      activeTodosCount: activeTodosCount,
+      activeTodosCount: activeTodosCount
     };
   }
 );
